51302fee7c333db83ccfc3a15ed5d114b651d753
Kevin McCarthy <me@kevinmccarthy.org>
2013-08-14 15:53:53 -1000

    dunno

---
+++ build.sh
@@ -0,0 +1,14 @@
+HOSTS=()
+
+for role in $(server_config/rgenvs.py -roles)
+do
+    HOSTS+=($(server_config/rgenvs.py -hosts -role=$role -env=development))
+done
+
+UNIQUE_HOSTS=$(echo "${HOSTS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+
+for host in $UNIQUE_HOSTS
+do
+    packer build -var "hostname=$host" template/template.json
+    mv output/tmp.box "output/$hostname.box"
+done

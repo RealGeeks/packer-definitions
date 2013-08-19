0eb30461c26fbeaf89c64b6fd6611a1123f87311
Kevin McCarthy <me@kevinmccarthy.org>
2013-08-19 12:48:52 -1000

    dunno

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+containsElement () {
+    for e in "${@:2}";
+    do
+        if [[ "$e" = "$1" ]] ; then
+            return 0;
+        fi
+    done;
+    return 1;
+}
+
+buildHost () {
+    packer build -var "hostname=$1" template/template.json
+    mv output/tmp.box "output/$1.box"
+}
+
 HOSTS=()
 
 for role in $(server_config/rgenvs.py -roles)
@@ -7,8 +24,18 @@ done
 
 UNIQUE_HOSTS=$(echo "${HOSTS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
-for host in $UNIQUE_HOSTS
-do
-    packer build -var "hostname=$host" template/template.json
-    mv output/tmp.box "output/$host.box"
-done
+if (( $# == 1 ))
+then
+    if containsElement "$1" $UNIQUE_HOSTS ; then
+        buildHost $1
+    else
+        echo "Sorry, \"$1\" was not one of $UNIQUE_HOSTS"
+    fi
+else
+
+    for host in $UNIQUE_HOSTS
+    do
+        buildHost $host
+    done
+
+fi
